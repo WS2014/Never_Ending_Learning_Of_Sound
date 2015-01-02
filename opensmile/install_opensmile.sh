3cce18205371d590b96b0aa67fab0d87ad3492c3
Rohan Badlani <rohan.badlani@gmail.com>
2015-01-02 11:35:54 +0530

    Install Script Modified. TODO: Write Script to generate MFCC for all Sound Samples.

--- opensmile/install_opensmile.sh
+++ opensmile/install_opensmile.sh
@@ -2,6 +2,36 @@
 
 result=${PWD}
 
+echo "Everything will be done in this directory."
+echo $result
+
+wget http://www.audeering.com/research-and-open-source/files/openSMILE-2.1.0.tar.gz
+tar -zxvf openSMILE-2.1.0.tar.gz
+sudo apt-get install build-essential libtool
+
+#PORTAUDIO INSTALLATION
+echo "run this in the directory where you want to install portaudio."
+wget http://www.portaudio.com/archives/pa_stable_v19_20140130.tgz
+sudo apt-get install libasound-dev
+tar -zvxf pa_stable_v19_20140130.tgz
+cd portaudio
+./configure && make
+sudo make install
+
+#back to openSmile
+
+cd ..
+
+cd openSMILE-2.1.0
+
+bash autogen.sh
+./configure
+sh buildWithInstalledPortAudio.sh
+./SMILExtract -h
+
+./SMILExtract -C config/MFCC12_E_D_A.conf -I ./../1.wav -O ./../mfcc.htk
+cd ..
+
 
 wget http://www.ee.ic.ac.uk/hp/staff/dmb/voicebox/voicebox.zip
 unzip voicebox.zip -d voicebox
