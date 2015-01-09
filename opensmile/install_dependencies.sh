29dac2f5cac3c6a79e1b571402fd793901a8670e
Rohan Badlani <rohan.badlani@gmail.com>
2015-01-10 00:03:00 +0530

    Added Running Instructions. Seperated installation dependencies and train_bashfile.sh

---
+++ opensmile/install_dependencies.sh
@@ -0,0 +1,9 @@
+#!usr/bash
+
+#code to install opensmile and dependencies
+bash install_opensmile.sh
+
+#install OpenMPI and Dependencies
+bash install_OpenMPI_Linux.sh
+bash install_MPI_C_Compiler.sh
+bash install_ParallelNetCDF.sh
