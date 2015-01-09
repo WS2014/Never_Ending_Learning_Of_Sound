629d0a4175600b95f516bac7b01f5b576d852efa
Rohan Badlani <rohan.badlani@gmail.com>
2015-01-09 21:12:14 +0530

    Updated splitter.py. Project Progress: Complete till SVM_Input

--- opensmile/splitter.py
+++ opensmile/splitter.py
@@ -3,77 +3,77 @@ import re
 import sys
 
 def splitter(input_path, input_path2, output_path):
-	with open(input_path + "/filelines.txt","r") as frame_per_file:
-		frame_count = []	
-		for line in frame_per_file:
-			num_list = re.split("\n", line)	
-			file_frame_count = int(num_list[0])
-			frame_count.append(file_frame_count)
-		frame_per_file.close()
-	
-	with open(input_path + "/filenames.txt", "r") as filenames:
-		file_name = []	
-		for line in filenames:
-			filename = re.split("\n", line)	
-			file_name.append(str(filename[0]))
-	
-	
-		filenames.close()
-
+		
 	with open(input_path + "/catgwise_numfiles.txt", "r") as userfile:
 		folder_name_count = []	
 		for line in userfile:
-			data_list = re.split(":|\n", line)
-			folder_name = ""
-			folder_name  = data_list[0]
-			number = int(data_list[1])
-		
+			splitted_line = line.split(",")
+			print splitted_line
+			category = splitted_line[0].split(":")[0]
+			category_frames = splitted_line[0].split(":")[1]
+			start = splitted_line[1].split(":")[0]
+			start_frames = splitted_line[1].split(":")[1]
+			end = splitted_line[2].split(":")[0]
+			end_frames = re.split(":|\n", splitted_line[2])[1]
+			
 			data = {}	
-			data['folder_name'] = str(folder_name)
-			data['count'] = number
-			print data
+			data['category'] = str(category)
+			data['category_frames'] = int(category_frames)
+			data['start'] = int(start_frames)
+			data['end'] = int(end_frames)
+			
 			folder_name_count.append(data)
 	
-		#print folder_name_count
 		userfile.close()
 
-
-
-	"""
-	for i in range(len(folder_name_count)-1, 0,-1):
-		folder_name_count[i]['count'] = folder_name_count[i]['count'] - folder_name_count[i-1]['count']
-		#print str(folder_name_count[i]['count'])
-	"""
-
-	#print folder_name_count
-
+	with open(input_path + "/filelines.txt","r") as frame_per_file:
+		frame_count = {}
+		for item in folder_name_count:
+			frame_count[item['category']] = []		
+		
+		for line in frame_per_file:
+			category_details = {}
+			num_list = re.split("\n", line)
+			category = num_list[0].split(",")[0].split("/")[0]
+			file_dict = {}
+			file_frame = int(num_list[0].split(",")[1])
+			file_name = num_list[0].split(",")[0].split("/")[1]
+			file_dict['file_frame'] = int(file_frame)
+			file_dict['file_name'] = file_name			
+			frame_count[category].append(file_dict)
+		frame_per_file.close()
+		print frame_count
+	
+	
+	
 	with open(input_path2 + "/normalized_text.txt.membership","r") as membership_file:
 		count = 0
 		i = 0
 		j = 0
 		for item in folder_name_count:
-			number = item['count']
-			print item['folder_name']
-			directory = os.path.join(output_path, item['folder_name'])
+			category_frames = item['category_frames']
+			category = item['category']
+			
+			directory = os.path.join(output_path, category)
 			if not os.path.exists(directory):
 				os.makedirs(directory)
-			while(number>0):
-				while(count<frame_count[i]):	
-					with open(str(directory + "/" + str(file_name[i])), "a") as userfile:
+			while(category_frames>0):
+				print frame_count[category]
+				while(count<frame_count[category][i]['file_frame']):	
+					with open(str(directory + "/" + str(frame_count[category][i]['file_name'])), "a") as userfile:
 						line = membership_file.readline()
-					
-						#print line
+						print line
 						userfile.write(line)
-						
 						count = count + 1
 			
 				userfile.close()
-				number = number - frame_count[i] 
-				i = i+1		
+				category_frames = category_frames - frame_count[category][i]['file_frame'] 
+				i = i+1	
 				count = 0
-        
+			i = 0
+				
 
-
+	
 if(len(sys.argv)!=4):
 	print "\nUSAGE: python splitter.py <folder_name_of_input_files> <folder_name_of_input_files2> <folder_name_of_output_folders>\n"
 else:
