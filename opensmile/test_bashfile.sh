5004f76e46163b93f1c11f6d8d9a48c4837920ad
ankitshah009 <ankit.tronix@gmail.com>
2015-01-11 02:17:11 +0530

    Minor addition for the autonorm_test.sh file to get the normalisation for the test files generated using accumulation code

--- opensmile/test_bashfile.sh
+++ opensmile/test_bashfile.sh
@@ -3,6 +3,10 @@
 #code to get the MFCC for all sample files for the test database.
 bash get_mfcc_for_samples_test.sh
 
+#code to get the accumulated MFCC File
 mkdir -p accumulated_folder_test
 python accumulation.py text_samples_folder_test accumulated_folder_test
+
+#code to normalize the accumulated file. The Mean and Variance are taken from the accumulated folder where the training data values are stored in mean_norm.txt and std_dev.txt
+bash autonorm_test.sh
 
