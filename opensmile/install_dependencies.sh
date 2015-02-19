d52d24a40e559541382396f39aa05ec0fadbf5bc
Ankitshah <ankit.tronix@gmail.com>
2015-02-19 23:27:41 +0530

    Update of the dependencies script for the same

--- opensmile/install_dependencies.sh
+++ opensmile/install_dependencies.sh
@@ -3,6 +3,7 @@
 #code to install opensmile and dependencies
 bash install_opensmile.sh
 
+bash install_python_oct2py.sh
 #install OpenMPI and Dependencies
 bash install_OpenMPI_Linux.sh
 bash install_MPI_C_Compiler.sh
