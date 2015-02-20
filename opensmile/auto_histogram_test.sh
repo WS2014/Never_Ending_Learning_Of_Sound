5ae4a280e2199b60e8b8c1c3cd0b660895910d27
Ankitshah <ankit.tronix@gmail.com>
2015-02-20 10:41:33 +0530

    File that generates a sliding window on the test file of audio and then histograms on each window as per the 200 clusters later which is normalised to generate a vertical_and_horizontal_normalised_histograms.txt

---
+++ opensmile/auto_histogram_test.sh
@@ -0,0 +1,47 @@
+#!usr/bash
+
+#supply the folder here
+current_dir=${PWD}
+sound_samples_folder="sound_samples_test"
+text_mfcc_folder="$current_dir/text_samples_folder_test"
+accumulation_folder="$current_dir/accumulated_folder_test"
+kmeans_output="$current_dir/kmeans_output_test"
+text_histogram_folder="$current_dir/text_histogram_folder_test"
+text_histogram_folder_norm="$current_dir/text_histogram_folder_norm_test"
+SVM_input="$current_dir/SVM_input_test"
+slides_80_10="$current_dir/slides_80_10"
+mkdir -p $text_mfcc_folder
+mkdir -p $accumulation_folder
+mkdir -p $text_histogram_folder
+mkdir -p $kmeans_output
+mkdir -p $text_histogram_folder_norm
+mkdir -p $SVM_input
+
+python slide.py $accumulation_folder/normalized_text.txt 80 10
+
+for file_histogram in `ls $slides_80_10/*`
+do
+	cp $file_histogram $text_histogram_folder/.
+done
+
+for txt_file in $text_histogram_folder/* 
+do
+         text_file_root=`echo $txt_file | sed -e s/text_histogram_folder/text_histogram_folder_norm/`  
+         text_file=`echo $text_file_root | sed -e s/$/.txt/`  
+         INP_FILE=$txt_file
+         OUT_FILE=$text_file  
+         echo "'$INP_FILE' '$OUT_FILE'"
+         octave -qf --eval "addpath('$current_dir'); cluster2bins('$INP_FILE','$OUT_FILE')"
+done
+python $current_dir/accumulation_test.py $text_histogram_folder_norm $SVM_input
+
+#for file_hist  in `ls $text_histogram_folder_norm/*`
+#do
+#	cat $text_histogram_norm/*.txt >> $SVM_input/accumulated_file.txt
+#done
+python $current_dir/horizontal_normalize.py $SVM_input/accumulated_file.txt $SVM_input
+
+octave -qf --eval "cd $SVM_input ;addpath('$current_dir'); mean_and_var_compute('$SVM_input/horizontal_normalized_histograms.txt'); cd $current_dir"
+
+python $current_dir/vertical_normalize.py $SVM_input/horizontal_normalized_histograms.txt $SVM_input/mean_norm.txt $SVM_input/std_dev_norm.txt $SVM_input
+
